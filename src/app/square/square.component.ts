1a549f36c3d6148014b055ce7aaba1f28277387c
demkantor <demkantor@gmail.com>
2020-05-07 14:44:16 -0500

    game functioning

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button *ngIf="!value">{{ value }}</button>
+    <button status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: [
+    'button { width: 100%; height: 100%; font-size: 5em; }'
+  ]
+})
+export class SquareComponent {
+  
+  @Input() value: 'X' | 'O';
+
+}
